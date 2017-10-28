f73c9522227ac7b0869562b3d8873e782705db8f
admin <admin@pfb01.cshl.edu>
2017-10-28 14:41:35 -0400

    updates user module

--- user.py
+++ user.py
@@ -9,17 +9,26 @@ class Prisoner(object):
 	
 	def __init__(self, name = 'Unknown', hp = 20, escapeStatus = False, difficulty = 'medium'):
 		self.name = name
+		if difficulty.upper() == 'easy'.upper():
+			hp = 40
+		elif difficulty.upper() == 'medium'.upper():
+			hp = 20
+		else:
+			hp = 10
 		self.hp = hp
 		self.escapeStatus = escapeStatus
 		self.difficulty = difficulty
 
 # Attack function generates random attack damage between 1 and 6 and adusts users hp from creatures attack value
 
-	def attack():
-		self.hp -= creature.Creature.attack
+	def attack(self):
+		self.hp -= creatures.Creatures.attack
 		strike = random.randrange(1,6)
 		return strike
 
-	def setHP(new_hp = None):
+	def setHP(self, new_hp = None):
 		self.hp = new_hp
+
+	def setescapeStatus(self, escapeStatus = False):
+		self.escapeStatus = escapeStatus
 
