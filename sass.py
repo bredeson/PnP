85f05672b173429431c69e39f081371478240ae3
admin <admin@pfb07.cshl.edu>
2017-10-28 16:18:24 -0400

    this is a functional function.

--- sass.py
+++ sass.py
@@ -3,7 +3,7 @@
 import random
 
 def sample_sass():
-    sassy_retorts=["You've entered an invalid response, you sassy dog.", "Stop avoiding decisions.", "Don't you backtalk me."]
+    sassy_retorts=["You've entered an invalid response, you sassy dog.", "Stop avoiding decisions.", "Don't you backtalk me.", "Use a keyboard much?"]
     index=random.randrange(len(sassy_retorts))
     return(sassy_retorts[index])
 
