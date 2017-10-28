4a78636aa39bcd5ad12fe08100389acfd30931ac
Jared Brewer <jared@jareds-mbp.cshl.edu>
2017-10-28 14:55:35 -0400

    Added dictionary integration to define features of creatures.

--- Creatures.py
+++ Creatures.py
@@ -13,6 +13,18 @@ class Goblin(Creatures):
 		self.attack = attack
 		
 creatures_list = open('sample_creatures_list', 'r')
+creature_dict = {}
 for line in creatures_list:
-	split = line.split('\t')
+	split = line.strip().split('\t')
+	creature_dict[split[0]] = split[1]	
+for key, value in creature_dict.items():
+	creature_dict[key] = value.split(',')
+	
+class PrisonGuard(Creatures):
+	def __init__(self, name = creature_dict['PrisonGuard'][0], 
+	hp = creature_dict['PrisonGuard'][1], attack = creature_dict['PrisonGuard'][2]):
+		self.name = name
+		self.hp = hp
+		self.attack = attack
+	
 	
