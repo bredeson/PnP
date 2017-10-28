da217442518e45dfb67b9c18a5dd446ab5bf7eac
admin <admin@pfb17.cshl.edu>
2017-10-28 19:28:06 -0400

    updated puzzles that works!!!

--- puzzles.py
+++ puzzles.py
@@ -4,13 +4,8 @@
 
 import re
 import random 
-import user
-
-class Puzzles(object)
-
-	def __init__(self, puzzle_type = None, completed = False):
-		self.puzzle_type = puzzle_type
-		self.completed = completed
+#import user
+import os
 
 #=====================================
 #Creating nested dictionary of puzzles
@@ -18,40 +13,71 @@ class Puzzles(object)
 
 master_puzzles = {
 		'Puzzle 1': {
-				'Question:':'xxxx',
-				'Answer:'  :'yyyy'
-		}
+				'Question:':'What is the latin name for white-faced capuchin monkey?',
+				'Answer:'  :'Cebus capucinus imitator'
+		},
 		'Puzzle 2': {
-				'Question:':'zzzz',
-				'Answer:'  :'qqqq'
-		}
+				'Question:':'What is 4 + 3?',
+				'Answer:'  :'7'
+		},
 		'Puzzle 3': {
-				'Question:':'aaaa',
-				'Answer:'  :'bbbb'
+				'Question:':'What day is it?',
+				'Answer:'  :'Saturday'
 		}
-}	
+}		
 
-#====================================
+#========================
+#Creating a class Puzzles
+#========================
+
+class Puzzles(object):
+
+	def __init__(self, puzzle_question = None, completed = False):
+		self.puzzle_question = random.choice(list(master_puzzles))
+		self.completed = completed
+
+#========================================================
 #Function that intakes user input
 #from command line (yes or no)
 #If yes, user enters if statement, a 
 #puzzle gets printed to the screen,
 #and user is prompted to enter answer
-#====================================
+#Usage:
+#	user_input = [yes] or [no] from command line
+#	user_answer = string from command line
+#	puzzle_question = generated within function, no 
+#						input from command line necessary
+#========================================================
 							
-	def do_puzzle(self, user_input = None, puzzle_question = None )
+	def do_puzzle(self, user_input = None):
 		try_counter = 3
-		if user_input == upper('yes'):
-			puzzle_question = random.choice(list(master_puzzles))
-			print(master_puzzles[puzzle_question]['Question:'])
-#user needs to imput answer to command line
-			if user_answer == master_puzzles[puzzle_num]['Answer:']:
-				print('{} is correct! You may proceed.'.format(master_puzzles[puzzle_num]['Answer:']))
-			else user_answer != master_puzzles[puzzle_num]['Answer:']:
-				try_counter -= 1
-				print('{} is incorrect! You have {} more tries.'.format(user_answer, try_counter) 
-				if try_counter = 0:
-					print('You have {} tries left. You have failed and are now dead.'.format(try_counter))
+		user_input = input('Would you like to do a puzzle? If you say [no], you will lose only X hp. If you say [yes], you could strike it rich, but if you fail, it could mean danger!\n\n')
+		
+
+		if user_input == 'yes':
+			print(master_puzzles[self.puzzle_question]['Question:']) 
+			
+			while try_counter > 0:
+				user_answer = input('What is your answer?\n')
+			
+				if user_answer == master_puzzles[self.puzzle_question]['Answer:']:
+					print('\n{} is correct! You may proceed.'.format(master_puzzles[self.puzzle_question]['Answer:']))
+					self.completed = True
+					return
+
+				elif user_answer != master_puzzles[self.puzzle_question]['Answer:']:
+					try_counter -= 1
+					print('{} is incorrect! You have {} more tries.'.format(user_answer, try_counter)) 
+					
+					if try_counter == 0:
+						print('You have {} tries left. Guess what? You\'re fucked.'.format(try_counter))
+						self.completed = False
+						return
+		
+		elif user_input == 'no':
+			print('\nPerhaps a wise choice! However, you are going to be penalized for your cowardice!')
+			return
+
 
 		 
 
