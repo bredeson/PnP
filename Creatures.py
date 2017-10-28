943fed2159711ec49f8f49a39db2f3829f78da2d
Jared Brewer <jared@jareds-mbp.cshl.edu>
2017-10-28 11:12:19 -0400

    First draft of the Creatures class script.

---
+++ Creatures.py
@@ -0,0 +1,21 @@
+import re, sys, time, rand
+
+class Creatures:
+    def __init__(self, name, attack, health):
+        self.name = name
+        self.attack = attack
+        self.health = health
+    def name(self):
+        name = zebrafish
+    def health(self):
+        health = 5
+    def attack(self):
+        attack = 2
+        
+class Dog(Creatures):
+    def name(self):
+        name = dog
+    def health(self):
+        health = 3
+    def attack(self):
+        attack = 1
