b7b6ef9bba77e68ce4c0434e91fb20a403d3db69
admin <admin@pfb07.cshl.edu>
2017-10-29 19:55:36 -0400

    Improved minor bugs

--- level2.py
+++ level2.py
@@ -26,7 +26,7 @@ while game_play==1:# the player enters the loop. they cannot escape the loop unt
     while player.hp>0:
         while player.level==2:
             input_s("As the daylight hits your face, you have a sudden flash of memory. The details are hazy, but you recall that you were imprisoned by a powerful wizard. Looking around you, you realize you are in a vaguely familiar courtyard - the courtyard of the wizard's castle! The outer wall is unscalable and the portcullis is up. This is your chance to find the wizard and solve the mystery of your imprisonment - and take your revenge!\n", player)
-            zookeepers_encounter(player)
+            zookeeper_encounter(player)
 
             entry_query=input_s("There is a window two floors up the castle wall and a main entrance. Do you [attempt the climb] or [enter the door]?\n",player)
             while entry_query not in ["attempt the climb", "enter the door", "rope"]:
@@ -40,7 +40,7 @@ while game_play==1:# the player enters the loop. they cannot escape the loop unt
                     input_s("Light-footed as a prancercize instructor, you alight on the floor of a cozy room smelling of roses - what a relief after that sewer! You look around and see a sorceress perusing a perl manual at a nearby table.", player)
                     sorc_approach=input_s("Do you approach the sorceress?[yes] or [no]\n", player)
                     while sorc_approach not in ["yes", "no"]:
-                        sorc_approach=input_s(sampl_sass(),player)
+                        sorc_approach=input_s(sample_sass(),player)
                     if sorc_approach=="yes":
                         input_s("'Hello young traveler!' she sings.\n 'My name is Sofia. Sit with me and gain some perls of wisdom.'", player)
                         player.intelligence+=2
@@ -51,7 +51,7 @@ while game_play==1:# the player enters the loop. they cannot escape the loop unt
                     
                     spa_query=input_s("There is a bubbling hottub in the spa. Do you sit in it?[yes] or [no]\n", player)
                     while spa_query not in ["yes", "no"]:
-                        spa_query=input_s(sampl_sass(),player)
+                        spa_query=input_s(sample_sass(),player)
                     if spa_query=="yes":
                         player.mana=3
                         print_s("You sit in the hottub and your mana is fully replenished to " + str(player.mana) + ".\n After your rejuvinating soak, you towel off and walk out the spa door. ")
@@ -71,7 +71,7 @@ while game_play==1:# the player enters the loop. they cannot escape the loop unt
                     player.mana+=1
             print_s("Bomb diggity. You emerge victorious from your latest escapade. Defeating this magical creature increases your mana to " + str(player.mana))
             print_s("At the end of the entrance hall is a magnificent pair of palatial staircases sweeping up to the next level. ")
-            stair_query=input_s("Do you go up the stairs?[yes] or [no]", player)
+            stair_query=input_s("Do you go up the stairs?[yes] or [no]\n", player)
             while stair_query not in ["yes", "no"]:
                 stair_query=input_s(sample_sass(), player)
             if stair_query=="no":
@@ -79,7 +79,8 @@ while game_play==1:# the player enters the loop. they cannot escape the loop unt
                     passive_actions=["A breeze blows gently through the windows.", "You hear the sound of birds chirping outside.", "A mouse scurrying along the wall rustles gently.", "You check your watch.", "All remains quiet in the entrance hall."]
                     index=random.randrange(len(passive_actions))
                     print_s(passive_actions[index])
-                    stair_query=input_s("Do you go up the staircase yet?[yes] or [no]")
+                    time.sleep(4)
+                    stair_query=input_s("Do you go up the staircase yet?[yes] or [no]\n", player)
             
             print_s("At the top of the stairs you enter a sumptuously decorated hallway. The thick carpet muffles the sound of your footsteps. You pass several creepy old paintings and notice a door cracked to your left.")
             easter_query=input_s("Do you open the door? [yes] or [no]\n", player)
