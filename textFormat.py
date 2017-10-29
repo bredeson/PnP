e4badf3ad7de373a46c76de79b4173ac7425f7f4
admin <admin@pfb01.cshl.edu>
2017-10-29 17:06:02 -0400

    fixing wrapping again

--- textFormat.py
+++ textFormat.py
@@ -26,9 +26,9 @@ def printHealthBar (current_hp, starting_hp, decimals = 1, length = 50, fill = '
 def input_s(text, user, color = ''):
 	if not color == '':
 		color_code = color_dict[color]
-		query= input(textwrap.fill(color_code + text + color_dict['end'], replace_whitespace = False))
+		query= input(textwrap.fill(color_code + text + color_dict['end'], replace_whitespace = False, drop_whitespace = False))
 	else:
-		query= input(textwrap.fill(text, replace_whitespace = False))
+		query= input(textwrap.fill(text, replace_whitespace = False, drop_whitespace = False))
 	while query=="status":
 		color_code = color_dict['green']
 		print(color_code + "\nName: {}\nHealth Points: {}\nDifficulty: {}\nAttack: {}\n".format(user.name, printHealthBar(user.hp, user._hp), user.difficulty, user.attack) + color_dict['end'])
@@ -36,7 +36,7 @@ def input_s(text, user, color = ''):
 	return(query)
 
 def print_s(text,color = ''):
-	text = textwrap.fill(text, replace_whitespace = False)
+	text = textwrap.fill(text, replace_whitespace = False, drop_whitespace = False)
 	if not color == '':
 		color_code = color_dict[color]
 		print(color_code + text + color_dict['end'])
