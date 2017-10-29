7d044af3e53ce4b7e8f31c16091c36b833c0749a
admin <admin@pfb03.cshl.edu>
2017-10-28 21:27:21 -0400

    Updated oyster_query

--- mitchell_encounter.py
+++ mitchell_encounter.py
@@ -3,10 +3,10 @@
 import creatures, random
 hp = 30
 #def first_combat(player):
-first_combat_query=input("You enter a dark room...an interesting smell looms in the air that makes your stomach rumble.\
- A giant oyster lies the in the middle of the room, but who knows how long it has been there?\
+oyster_query=input("As you enter the sewers...an interesting smell looms in the air that makes your stomach rumble.\
+ A giant oyster lies the on the sides, but who knows how long it has been there?\
  Do you [eat] the oyster or [keep moving]?\n")
-if first_combat_query=="eat":
+if oyster_query=="eat":
 	oyster = random.randint(-5,5)
 	hp += oyster
 	if oyster > 0:
@@ -15,7 +15,7 @@ if first_combat_query=="eat":
 		print('The oyster seemed to do very little, but at least nothing bad has happened...yet')
 	else:
 		print("The oyster was foul, and your stomach lurches. You lose", oyster, 'hp. Your total hp is', hp)
-elif first_combat_query=="keep moving":
+elif oyster_query=="keep moving":
         print("You continue onward, ignoring the pang of hunger. Better not to risk it.")
        # decision_counter=2
    # else:
