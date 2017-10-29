fa414e014dfc35a4d3ab6e80d4c11a3ad265f0fd
admin <admin@pfb03.cshl.edu>
2017-10-28 20:47:29 -0400

    changed mitchell_enocunter to be a risky oyster?

--- mitchell_encounter.py
+++ mitchell_encounter.py
@@ -3,24 +3,20 @@
 import creatures, random
 hp = 30
 #def first_combat(player):
-first_combat_query=input("You enter a dark room. As you walk through, you step in a puddle. Crimson blood drips from your boot. .\
- You see a dead guard torn apart. Do you [search] his body or say a prayer and [keep moving]?\n")
-if first_combat_query=="search":
-	print("You find a vial filled with a mysterious liquid.\n")
-	drink_query=input('The bright blue liquid sloshes around the vial. Do you [drink] it or [leave] it.')
-	if drink_query=='drink':
-		potion = random.randint(-5,5)
-		hp += potion
-		if potion > 0:
-			print("You feel refreshed and gain", potion, 'hp. Your total hp is', hp)
-		elif potion ==0:
-			print('The potion seemed to have no effect...')
-		else:
-			print("Your stomach lurches. You lose", potion, 'hp. Your total hp is', hp)
+first_combat_query=input("You enter a dark room...an interesting smell looms in the air that makes your stomach rumble.\
+ A giant oyster lies the in the middle of the room, but who knows how long it has been there?\
+ Do you [eat] the oyster or [keep moving]?\n")
+if first_combat_query=="eat":
+	oyster = random.randint(-5,5)
+	hp += oyster
+	if oyster > 0:
+		print("The oyster was still good! Your hunger is sated. You gain", oyster, 'hp. Your total hp is', hp)
+	elif oyster ==0:
+		print('The oyster seemed to do very little, but at least nothing bad has happened...yet')
 	else:
-		print('You ignore the liquid')
+		print("The oyster was foul, and your stomach lurches. You lose", oyster, 'hp. Your total hp is', hp)
 elif first_combat_query=="keep moving":
-        print("You continue onward, pushing the thought of death out of your mind.")
+        print("You continue onward, ignoring the pang of hunger. Better not to risk it.")
        # decision_counter=2
    # else:
     #    print(sass.sample_sass(), '\n')
