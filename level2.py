65dfe24de46e3f6a1e4519eb0522fa03622a4894
admin <admin@pfb07.cshl.edu>
2017-10-29 19:44:08 -0400

    Moved up zookeeper encounter. Added stair query to sit peacefully in the entrance hall.

--- level2.py
+++ level2.py
@@ -26,6 +26,7 @@ while game_play==1:# the player enters the loop. they cannot escape the loop unt
     while player.hp>0:
         while player.level==2:
             input_s("As the daylight hits your face, you have a sudden flash of memory. The details are hazy, but you recall that you were imprisoned by a powerful wizard. Looking around you, you realize you are in a vaguely familiar courtyard - the courtyard of the wizard's castle! The outer wall is unscalable and the portcullis is up. This is your chance to find the wizard and solve the mystery of your imprisonment - and take your revenge!\n", player)
+            zookeepers_encounter(player)
 
             entry_query=input_s("There is a window two floors up the castle wall and a main entrance. Do you [attempt the climb] or [enter the door]?\n",player)
             while entry_query not in ["attempt the climb", "enter the door", "rope"]:
@@ -57,21 +58,38 @@ while game_play==1:# the player enters the loop. they cannot escape the loop unt
                     elif spa_query=="no":
                         print_s("You walk right past the enticing hottub you so stubbornly rejected.")
                     print_s("You exit into a sumptuously decorated hallway. Your feet sinking into the plush carpet, you notice a door cracked to your left.")
-                    easter_query=input_s("Do you open the door? [yes] or [no]\n", player)
-                    while easter_query not in ["yes", "no"]:
-                        easter_query=input_s(sample_sass(), player)
-                    if easter_query=="yes":
-                        print_s("enter drew's encounter here")
-                    elif easter_query=="no":
-                        print_s("You continue down the hallway")
             elif entry_query=="enter the door":
                 opponent=creatures.Magic()
-                print_s("You strut confidently through the main castle door. You enter an impressively portioned entrance hall lit by sunlight streaming through the tall mullioned windows. You briefly admire the marble statues and tapestries lining the walls before you see a " + opponent + " taking a fighting stance ten meters in front of you.")
-                print_s(opponent.art())
+                print_s("You strut confidently through the main castle door. You enter an impressively portioned entrance hall lit by sunlight streaming through the tall mullioned windows. You briefly admire the marble statues and tapestries lining the walls before you see a " + opponent.name + " taking a fighting stance ten meters in front of you.")
+                time.sleep(5)
+                print(opponent.art())
                 player.combat(opponent)
-                print_s("story")
+                if player.hp<=0:
+                    print_s("You made it so far...but this is how you die.")
+                    break
+                else:
+                    player.mana+=1
+            print_s("Bomb diggity. You emerge victorious from your latest escapade. Defeating this magical creature increases your mana to " + str(player.mana))
+            print_s("At the end of the entrance hall is a magnificent pair of palatial staircases sweeping up to the next level. ")
+            stair_query=input_s("Do you go up the stairs?[yes] or [no]", player)
+            while stair_query not in ["yes", "no"]:
+                stair_query=input_s(sample_sass(), player)
+            if stair_query=="no":
+                while(stair_query=="no"):
+                    passive_actions=["A breeze blows gently through the windows.", "You hear the sound of birds chirping outside.", "A mouse scurrying along the wall rustles gently.", "You check your watch.", "All remains quiet in the entrance hall."]
+                    index=random.randrange(len(passive_actions))
+                    print_s(passive_actions[index])
+                    stair_query=input_s("Do you go up the staircase yet?[yes] or [no]")
+            
+            print_s("At the top of the stairs you enter a sumptuously decorated hallway. The thick carpet muffles the sound of your footsteps. You pass several creepy old paintings and notice a door cracked to your left.")
+            easter_query=input_s("Do you open the door? [yes] or [no]\n", player)
+            while easter_query not in ["yes", "no"]:
+                easter_query=input_s(sample_sass(), player)
+            if easter_query=="yes":
+                print_s("enter drew's encounter here")
+            elif easter_query=="no":
+                print_s("You continue down the hallway")
                 #have a hallway encounter
-            zookeeper_encounter(player)
             print_s("Add a segue here to get into Shasta's encounter")
             print_s("Shasta's encounter")
             player.level=3
