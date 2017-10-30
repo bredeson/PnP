36fcf2c84266c9c0ecf5e5d20a80603ff96ad754
admin <admin@pfb07.cshl.edu>
2017-10-29 21:30:51 -0400

    Fixed brownie encounter

--- brownie_encounter.py
+++ brownie_encounter.py
@@ -8,10 +8,10 @@ def brownie_encounter(player):
 	brownie_query=input_s("As you ascend the palatial stairway...an interesting smell looms in the air that makes your stomach rumble. A giant brownie lies to one side, but you have some concerning recent memories of a dodgy oyster. Do you [eat] the brownie or [keep moving]?\n", player)
 	while str(brownie_query) not in ["eat", "keep moving"]:
 		brownie_query=input_s(sample_sass(), player)
-
+	brownie=random.randrange(1,5)
 	if brownie_query=="eat":
 		print_s("The brownie was still good! Your hunger is sated. You gain"+ str(brownie)+ 'hp. Your total hp is'+ str(player.hp))
-		player.hp+=1
+		player.hp+=brownie
 				
 	elif brownie_query=="keep moving":
 		print_s("You continue onward, ignoring the pang of hunger. Better not to risk it.")
