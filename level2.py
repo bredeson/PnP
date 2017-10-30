fdfe5ef9af1363439882d7f3385d7384d21e73d0
admin <admin@pfb07.cshl.edu>
2017-10-30 11:53:26 -0400

    functionalized what happens when you walk in the door to add the option to decide to enter the door after you've attempted to climb.

--- level2.py
+++ level2.py
@@ -12,7 +12,7 @@ from zookeeper_encounter import zookeeper_encounter
 from brownie_encounter import brownie_encounter
 from jared_encounter import shark_game
 from easter_egg import easter_egg
-
+from walk_in import walk_in
 
 #initialize global variables                                                                               
 
@@ -32,8 +32,8 @@ while game_play==1:# the player enters the loop. they cannot escape the loop unt
             input_s("As the daylight hits your face, you feel the warmth sink into your soul and your health points increase to " + str(player.hp) + ". You have a sudden flash of memory: the details are hazy, but you recall that you were imprisoned by a powerful wizard. Looking around you, you realize you are in a vaguely familiar courtyard - the courtyard of the wizard's castle! This is your chance to find the wizard and solve the mystery of your imprisonment - and take your revenge!\n", player)
             zookeeper_encounter(player)
 
-            entry_query=input_s("You exit the menagerie and look around the courtyard. The outer wall is unscalable and the portcullis is down. There is a window two floors up the castle wall and a main entrance. Do you attempt to [climb] the wall or [enter] the door?\n",player)
-            while entry_query not in ["climb", "enter", "rope"]:
+            entry_query=input_s("You exit the menagerie and look around the courtyard. The outer wall is unscalable and the portcullis is down. There is a window two floors up the castle wall and a main entrance. Do you attempt to [climb] the wall or [walk] in the door?\n",player)
+            while entry_query not in ["climb", "walk", "rope"]:
                 entry_query=input_s(sample_sass(),player, "purple")
             if entry_query=="climb" or entry_query=="rope":
                 success=attempt_climb(player, entry_query)
@@ -62,31 +62,16 @@ while game_play==1:# the player enters the loop. they cannot escape the loop unt
                     elif spa_query=="no":
                         input_s("You walk right past the enticing hottub. Really?", player)
                     print_s("You exit into a sumptuously decorated hallway. Your feet sinking into the plush carpet, you notice a door cracked to your left.")
-            elif entry_query=="enter":
-                opponent=creatures.Magic()
-                print_s("You strut confidently through the main castle door. You enter an impressively portioned entrance hall lit by sunlight streaming through the tall mullioned windows. You briefly admire the marble statues and tapestries lining the walls.") 
-                input_s("You see a " + opponent.name + " taking a fighting stance ten meters in front of you.", player)
-                print(opponent.art())
-                player.combat(opponent)
+                elif success==2:
+                    walk_in(player)
+                    if player.hp<=0:
+                        print_s("You made it so far...but this is how you die.")                
+                        break
+            elif entry_query=="walk":
+                walk_in(player)
                 if player.hp<=0:
-                    print_s("You made it so far...but this is how you die.")
+                    print_s("You made it so far...but this is how you die.")                
                     break
-                else:
-                    player.mana+=1
-                input_s("Bomb diggity. You emerge victorious from your latest escapade. Defeating this magical creature increases your mana to " + str(player.mana)+ ".\n", player)
-                print_s("At the end of the entrance hall is a magnificent pair of staircases sweeping up to the next level. ")
-                stair_query=input_s("Do you go up the stairs?[yes] or [no]\n", player)
-                while stair_query not in ["yes", "no"]:
-                    stair_query=input_s(sample_sass(), player, "purple")
-                if stair_query=="no":
-                    while(stair_query=="no"):
-                        passive_actions=["A breeze blows gently through the windows.", "You hear the sound of birds chirping outside.", "A mouse scurrying along the wall rustles gently.", "You check your watch.", "All remains quiet in the entrance hall.", "You think about how long it must take to chisel a sculpture.", "You watch an ant crawl by on the floor.", "You contemplate what your life could have been if you hadn't gone to prison.", "You spy a new oil painting on an easel and watch it dry. " ]
-                        index=random.randrange(len(passive_actions))
-                        print_s(passive_actions[index])
-                        time.sleep(5)
-                        stair_query=input_s("Do you go up the staircase yet?[yes] or [no]\n", player)
-                brownie_encounter(player)
-                input_s("At the top of the stairs you enter a sumptuously decorated hallway. The thick carpet muffles the sound of your footsteps. You pass several creepy old paintings and notice a door cracked to your left.", player)
             easter_query=input_s("Do you open the door? [yes] or [no]\n", player)
             while easter_query not in ["yes", "no"]:
                 easter_query=input_s(sample_sass(), player, "purple")
@@ -108,7 +93,7 @@ while game_play==1:# the player enters the loop. they cannot escape the loop unt
                 input_s("Filled with gratitude, you return to your home as Baron(ess) of Hershey Manor, never to abuse sudo again.", player)
             else:
                 print_s("Simon shakes his head in disappointment./n 'I can see you've learned nothing from this experience. I'm afraid there's only one place for sloppy coders in this universe.'")
-                player.level=3
+                player.level=1
                 
             player.level=3
         player.hp=0
